0d5944d10e8ef1cd28ef4aa3a13fc6caa9b49fd2
Sharvari <161017777+sharvariiss@users.noreply.github.com>
2025-12-12 17:03:38 +0100

    3rd commit .. changes in flex

--- medical_crawler/medical_crawler/spiders/flexikon_spider.py
+++ medical_crawler/medical_crawler/spiders/flexikon_spider.py
@@ -37,6 +37,9 @@
 #             "images": image_urls,
 #         }
 
+# 
+
+
 import scrapy
 from urllib.parse import urljoin
 
@@ -47,81 +50,78 @@ class FlexikonAsaSpider(scrapy.Spider):
         "https://flexikon.doccheck.com/de/Acetylsalicyls%C3%A4ure"
     ]
 
-    def extract_section(self, response, headline_text, level="h2"):
+    def get_section(self, response, section_id):
         """
-        Extracts all text between a given headline (<h2> or <h3>) 
-        and the next headline of the same level.
+        Extract all text belonging to a MediaWiki section by span id.
         """
-        # h2 / h3 -> span.mw-headline text='...'
         xpath = f"""
-        //{level}[span[@class='mw-headline' and normalize-space(text())='{headline_text}']]
+        //span[@id='{section_id}']
+        /parent::h2
         /following-sibling::*[
-            preceding-sibling::{level}[1]/span[@class='mw-headline' and normalize-space(text())='{headline_text}']
+            not(self::h2)
         ]
         """
         nodes = response.xpath(xpath)
-        # Stop at the next h2/h3 – the XPath above already does that by condition
         texts = []
         for node in nodes:
-            # We don't want to accidentally run into the next headline level
-            tag = node.root.tag if hasattr(node.root, "tag") else None
-            if tag in ("h2", "h3"):
+            if node.root.tag == "h2":
                 break
-            part = " ".join(node.xpath(".//text()").getall())
-            part = " ".join(part.split())
-            if part:
-                texts.append(part)
+            text = " ".join(node.xpath(".//text()").getall())
+            text = " ".join(text.split())
+            if text:
+                texts.append(text)
+        return "\n\n".join(texts) if texts else None
+
+    def get_subsection(self, response, section_id):
+        xpath = f"""
+        //span[@id='{section_id}']
+        /parent::h3
+        /following-sibling::*[
+            not(self::h3)
+        ]
+        """
+        nodes = response.xpath(xpath)
+        texts = []
+        for node in nodes:
+            if node.root.tag == "h3":
+                break
+            text = " ".join(node.xpath(".//text()").getall())
+            text = " ".join(text.split())
+            if text:
+                texts.append(text)
         return "\n\n".join(texts) if texts else None
 
     def parse(self, response):
         base = "https://flexikon.doccheck.com"
 
-        # general info
-        title = response.css("h1#firstHeading::text").get()
-        image_urls = response.css(".mw-parser-output img::attr(src)").getall()
-        image_urls = [urljoin(base, u) for u in image_urls]
-
-        # main sections (German headings on the page)
-        definition         = self.extract_section(response, "Definition", "h2")
-        history            = self.extract_section(response, "Geschichte", "h2")
-        chemistry          = self.extract_section(response, "Chemie", "h2")
-        mechanism          = self.extract_section(response, "Wirkmechanismus", "h2")
-        mechanism_cox1     = self.extract_section(response, "Wirkung auf COX-1", "h3")
-        mechanism_cox2     = self.extract_section(response, "Wirkung auf COX-2", "h3")
-        pharmacokinetics   = self.extract_section(response, "Pharmakokinetik", "h2")
-        indications        = self.extract_section(response, "Indikationen", "h2")
-        dosage             = self.extract_section(response, "Dosierung", "h2")
-        prescription       = self.extract_section(response, "Verschreibungspflicht", "h2")
-        side_effects       = self.extract_section(response, "Nebenwirkungen", "h2")
-        interactions       = self.extract_section(response, "Wechselwirkungen", "h2")
-        contraindications  = self.extract_section(response, "Kontraindikationen", "h2")
-        warnings           = self.extract_section(response, "Warnhinweise", "h2")
-        toxicity           = self.extract_section(response, "Toxizität", "h2")
-        podcast            = self.extract_section(response, "Podcast", "h2")
-        literature         = self.extract_section(response, "Literatur", "h2")
-        image_source       = self.extract_section(response, "Bildquelle", "h2")
-
         yield {
             "source": "flexikon",
             "url": response.url,
-            "title": title,
-            "definition": definition,
-            "history": history,
-            "chemistry": chemistry,
-            "mechanism_of_action": mechanism,
-            "mechanism_cox1": mechanism_cox1,
-            "mechanism_cox2": mechanism_cox2,
-            "pharmacokinetics": pharmacokinetics,
-            "indications": indications,
-            "dosage": dosage,
-            "prescription_requirement": prescription,
-            "side_effects": side_effects,
-            "interactions": interactions,
-            "contraindications": contraindications,
-            "warnings": warnings,
-            "toxicity": toxicity,
-            "podcast": podcast,
-            "literature": literature,
-            "image_source": image_source,
-            "images": image_urls,
+            "title": response.css("h1#firstHeading::text").get(),
+
+            "definition": self.get_section(response, "Definition"),
+            "history": self.get_section(response, "Geschichte"),
+            "chemistry": self.get_section(response, "Chemie"),
+            "mechanism_of_action": self.get_section(response, "Wirkmechanismus"),
+
+            "mechanism_cox1": self.get_subsection(response, "Wirkung_auf_COX-1"),
+            "mechanism_cox2": self.get_subsection(response, "Wirkung_auf_COX-2"),
+
+            "pharmacokinetics": self.get_section(response, "Pharmakokinetik"),
+            "indications": self.get_section(response, "Indikationen"),
+            "dosage": self.get_section(response, "Dosierung"),
+            "prescription_requirement": self.get_section(response, "Verschreibungspflicht"),
+            "side_effects": self.get_section(response, "Nebenwirkungen"),
+            "interactions": self.get_section(response, "Wechselwirkungen"),
+            "contraindications": self.get_section(response, "Kontraindikationen"),
+            "warnings": self.get_section(response, "Warnhinweise"),
+            "toxicity": self.get_section(response, "Toxizität"),
+            "podcast": self.get_section(response, "Podcast"),
+            "literature": self.get_section(response, "Literatur"),
+            "image_source": self.get_section(response, "Bildquelle"),
+
+            "images": [
+                urljoin(base, img)
+                for img in response.css(".mw-parser-output img::attr(src)").getall()
+            ]
         }
